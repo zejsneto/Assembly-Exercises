90aa5dd1ecedc055c8bec6d5d212fa11bfdcdcfc
José Joaquim da Silva Neto <zeneto2508@gmail.com>
2023-02-14 12:05:34 -0300

    Add files via upload

---
+++ Lab03/Ex03.asm
@@ -0,0 +1,16 @@
+ORG 0050h
+DB -9
+ORG 0060h
+DB 25
+ORG 0h
+CLR A
+MOV DPTR, #0050h
+MOVC A,@A+DPTR
+MOV R2,A
+CLR A
+MOV DPTR, #0060h
+MOVC A,@A+DPTR
+SUBB A,R2
+MOV R5,A
+SJMP $
+;A resposta da conta está correta, pois 25-(-9) = 34, 34 em hexadecimal é 22.
---
+++ Lab03/Ex04.asm
@@ -0,0 +1,6 @@
+ORG 0100h
+DB "jose"
+
+ORG 0h
+
+;Os valores estão corretos. O padrão utilizado é o ASC II para armazenar nas memórias.
